086c88035d30ec53d19e49ddb062d998b3725e1e
Manuel <75450615+vlk-yr@users.noreply.github.com>
2021-11-23 15:45:21 -0300

    Update NLURoutes.js

--- routes/NLURoutes.js
+++ routes/NLURoutes.js
@@ -9,10 +9,9 @@ import {get_nlu_structure,
 const router = express.Router();
 
 router.get('/nlu_structures', get_nlu_structure);
-router.get('/nlu_structures_name', get_nlu_structure_name);
+router.get('/nlu_structure_name', get_nlu_structure_name);
 router.post('/nlu_structure', add_nlu_structure);
 router.put('/nlu_structure', put_nlu_structure);
 router.delete('/nlu_structure/:id', delete_nlu_structure);
 
-export default router;
-
+export default router
