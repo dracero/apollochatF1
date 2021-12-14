303c8ede5b46deb8ba020b8ad5666d1b45e2f345
unknown <diego.racero@gmail.com>
2021-12-13 21:21:23 -0300

    final

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-import express from "express";
 import mongoose from "mongoose";
-import nluRouter from "./routes/NLURoutes.js";
-import cors from 'cors';
 import typeDefs from "./schema.js";
 import resolvers from "./resolvers.js";
 import { ApolloServer } from 'apollo-server';
-//const app = express();
 
-//app.use(cors());
-//app.use(express.json());
-//solo poner useNewUrlParser y useUnifiedTopology porque las otras están deprecadas
 mongoose.connect(
   "mongodb+srv://euge:1234@cluster0.b2f7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false,
-    useCreateIndex: true //hay que agregar esto para que sea único el nombre
+    useUnifiedTopology: true
   }
 );
 //si la base de datos no existe Mongo la crea
@@ -33,19 +24,8 @@ async function startApolloServer(typeDefs, resolvers) {
       credentials: true}
   });
 
-  /*
-
-  const { url, port } = await server.listen({port: PORT});
-  console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-  `);
-
-  */
-
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
+  
+  server.listen({port: PORT}).then(({ url }) => {
     console.log(`
     🚀  Server is running
     📭  Query at ${url}
